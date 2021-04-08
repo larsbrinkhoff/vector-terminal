caad3a14fcecc5a11c58e373a2b64fd01d53f44d
Lars Brinkhoff <lars@nocrew.org>
2021-04-08 20:47:03 +0200

    Keyboard input.

--- js/vec.js
+++ js/vec.js
@@ -5,6 +5,23 @@ var fullscreen = false;
 var current_x = -1;
 var current_y = -1;
 
+var keymap = [ null, null, null, null, null, null, null, null,
+               null, null, null, null, null, null, null, null,
+               null, null, null, null, null, null, null, null,
+               null, null, null, null, null, null, null, null,
+               0x2C, 0x1E, 0x34, 0x20, 0x21, 0x22, 0x24, 0x34, 
+               0x26, 0x27, 0x25, 0x2E, 0x36, 0x2D, 0x37, 0x38,
+               0x27, 0x1E, 0x1F, 0x20, 0x21, 0x22, 0x23, 0x24,
+               0x25, 0x26, 0x33, 0x33, 0x36, 0x2E, 0x37, 0x38,
+               null, null, null, null, null, null, null, null,
+               null, null, null, null, null, null, null, null,
+               null, null, null, null, null, null, null, null,
+               null, null, null, 0x2F, 0x31, 0x30, 0x23, 0x2D,
+               0x1F, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A,
+               0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x12,
+               0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A,
+               0x1B, 0x1C, 0x1D, 0x2F, 0x31, 0x30, 0x31, null ];
+
 function fullscreenIII() {
     fullscreen = !fullscreen;
     if (fullscreen) {
@@ -58,7 +75,6 @@ function receiveLine() {
         y1 = (bytes[2] << 8) | bytes[3];
         x2 = (bytes[4] << 8) | bytes[5];
         y2 = (bytes[6] << 8) | bytes[7];
-        console.log(">> Line " + x1 + "," + y1 + " - " + x2 + "," + y2);
         ctx.strokeStyle = "#00FF00";
         ctx.beginPath();
         ctx.moveTo(x1, y1);
@@ -131,12 +147,64 @@ window.onload = function() {
     connectIII();
 }
 
+function sendKey(code, state) {
+    if (!ws)
+        return;
+    if (!code)
+        return;
+    var buf = [1, state, code];
+    ws.send(buf);
+    console.log("Key sent: " + buf);
+}
+
 function keydown(ev) {
     console.log("Key down: " + ev);
+    if (ws)
+        ev.preventDefault();
+    var keysym = getKeysym(ev);
+    console.log("Keysym: " + keysym);
+    switch(keysym) {
+    case 0xFFFF: //Delete -> Rubout
+    case 0xFF08: sendKey(0x2A, 1); break; //Backspace -> Rubout
+    case 0xFF09: sendKey(0x2B, 1); break; //Tab -> Tab
+    case 0xFF0D: sendKey(0x28, 1); break; //Return -> Return
+    case 0xFF1B: sendKey(0, 1); break; //Esc -> Altmode
+    case 0xFFBE: sendKey(0, 1); break; //F1 -> CALL
+    case 0xFFBF: sendKey(0, 1); break; //F2 -> ESC
+    case 0xFFC8: fullscreenTV(); break; //F11 -> Fullscreen
+    case 0xFFE1: sendKey(0xE1, 1); break;
+    case 0xFFE3: sendKey(0xE0, 1); break; //Control -> Control
+    case 0xFFE9: sendKey(0xE2, 1); break; //Alt -> Meta
+    default:
+        if (keysym < keymap.length)
+            sendKey(keymap[keysym], 1);
+        break;
+    }
 }
 
 function keyup(ev) {
     console.log("Key up: " + ev);
+    if (ws)
+        ev.preventDefault();
+    var keysym = getKeysym(ev);
+    console.log("Keysym: " + keysym);
+    switch(keysym) {
+    case 0xFFFF: //Delete -> Rubout
+    case 0xFF08: sendKey(0x2A, 2); break; //Backspace
+    case 0xFF09: sendKey(0x2B, 2); break; //Tab
+    case 0xFF0D: sendKey(0x28, 2); break; //Return
+    case 0xFF1B: sendKey(0, 2); break; //Esc
+    case 0xFFBE: sendKey(0, 2); break; //F1 -> CALL
+    case 0xFFBF: sendKey(0, 2); break; //F2 -> ESC
+    case 0xFFC8: fullscreenTV(); break; //F11 -> Fullscreen
+    case 0xFFE1: sendKey(0xE1, 2); break;
+    case 0xFFE3: sendKey(0xE0, 2); break; //Control -> Control
+    case 0xFFE9: sendKey(0xE2, 2); break; //Alt -> Meta
+    default:
+        if (keysym < keymap.length)
+            sendKey(keymap[keysym], 2);
+        break;
+    }
 }
 
 function button() {
--- sdl/defs.h
+++ sdl/defs.h
@@ -9,7 +9,9 @@ enum event {
 
 extern void display (void);
 extern int network (void *);
+extern int transmit (void *data, int n);
 extern void draw_line (int x1, int y1, int x2, int y2);
 extern void draw_point (int x, int y);
 extern void fatal (char *format, ...);
+
 extern FILE *debug;
